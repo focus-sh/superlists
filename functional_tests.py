12b381009ef0fb3b9d301cfbf21b8fe6788c0d94
zhibin.wang <smile0916>
2020-03-02 17:46:41 +0800

    Add functional tests for multiline rows in to-do list

--- functional_tests.py
+++ functional_tests.py
@@ -19,22 +19,35 @@ class NewVisitorTest(unittest.TestCase):
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do', header_text)
 
-		inputBox = self.browser.find_element_by_id('id_new_item')
+		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertEqual(
-			inputBox.get_attribute('placeholder'),
+			inputbox.get_attribute('placeholder'),
 			'Enter a to-do item'
 		)
 
-		inputBox.send_keys('Buy peacock feathers')
+		inputbox.send_keys('Buy peacock feathers')
 
-		inputBox.send_keys(Keys.ENTER)
+		inputbox.send_keys(Keys.ENTER)
 		time.sleep(1)
 
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertTrue(
 			any(row.text == '1: Buy peacock feathers' for row in rows),
-			"New to-do item did not appear in table"
+			f'New to-do item did not appear in table. Contents were: \n{table.text}'
+		)
+
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.assertIn(
+			'2: Use peacock feathers to make a fly',
+			[row.text for row in rows]
 		)
 
 		self.fail('Finish the test!')
--- lists/tests.py
+++ lists/tests.py
@@ -5,9 +5,14 @@ from django.http import HttpRequest
 
 class HomePageTest(TestCase):
 
-	def test_root_url_resolves_to_home_page_view(self):
-		found = resolve('/')
-		self.assertEqual(found.func, home_page)
+	def test_uses_home_teplate(self):
+		response = self.client.get('/')
+		self.assertTemplateUsed(response, 'home.html')
+
+	def test_can_save_a_POST_request(self):
+		response = self.client.post('/', data={'item_text': 'A new list item'})
+		self.assertIn('A new list item', response.content.decode())
+		self.assertTemplateUsed(response, 'home.html')
 
 	def test_home_page_return_correct_html(self):
 		response = self.client.get('/')
