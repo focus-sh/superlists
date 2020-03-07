7a7f6f27c39c92d38019dc733b4bcf0e845ad49e
focus-sh <wangzhibin2013@126.com>
2020-03-07 15:11:49 +0800

    Moved Fts into their own individual files

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,8 +1,16 @@
 from .base import FunctionalTest
-
+from selenium.webdriver.common.keys import Keys
 
 class ItemValidationTest(FunctionalTest):
 
 	def test_cannot_add_empty_lsit_items(self):
-		self.fail('write me!')
+		self.browser.get(self.live_server_url)
+		self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+
+		self.assertEqual(
+			self.browser.find_element_by_css_selector('.has-error').text,
+			"You can't have an empty list item"
+		)
+
+		self.fail('finish this test!')
 
