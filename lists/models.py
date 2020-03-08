8a012f14ab2f9f576fac879bd63428f7d11a4b5e
focus-sh <wangzhibin2013@126.com>
2020-03-08 21:24:37 +0800

    Refactor the format of source code

--- lists/models.py
+++ lists/models.py
@@ -1,22 +1,20 @@
 from django.db import models
 from django.urls import reverse
 
+
 class List(models.Model):
-	
-	def get_absolute_url(self):
-		return reverse('view_list', args=[self.id])
+
+    def get_absolute_url(self):
+        return reverse('view_list', args=[self.id])
 
 
 class Item(models.Model):
-	text = models.TextField(default='')
-	list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
+    text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
 
-	class Meta:
-		ordering = ('id',)
-		unique_together = ('list', 'text')
+    class Meta:
+        ordering = ('id',)
+        unique_together = ('list', 'text')
 
-	def __str__(self):
-		return self.text
-
-
-		
+    def __str__(self):
+        return self.text
--- lists/views.py
+++ lists/views.py
@@ -1,29 +1,30 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django.core.exceptions import ValidationError
-from lists.models import Item, List
+
 from lists.forms import ItemForm, ExistingListItemForm
+from lists.models import List
+
 
 def home_page(request):
-	return render(request, 'home.html', {'form': ItemForm()})
+    return render(request, 'home.html', {'form': ItemForm()})
+
 
 def view_list(request, list_id):
-	list_ = List.objects.get(id=list_id)
-	form = ExistingListItemForm(for_list=list_)
-	if request.method == 'POST':
-		form = ExistingListItemForm(for_list=list_, data=request.POST)
-		if form.is_valid():
-			form.save()
-			return redirect(list_)
-	
-	return render(request, 'list.html', {'list': list_,  'form': form})
+    list_ = List.objects.get(id=list_id)
+    form = ExistingListItemForm(for_list=list_)
+    if request.method == 'POST':
+        form = ExistingListItemForm(for_list=list_, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(list_)
+
+    return render(request, 'list.html', {'list': list_, 'form': form})
+
 
 def new_list(request):
-	form = ItemForm(data=request.POST)
-	if form.is_valid():
-		list_ = List.objects.create()
-		form.save(for_list=list_)
-		return redirect(list_)
-	else:
-		return render(request, 'home.html', {"form": form})
-
+    form = ItemForm(data=request.POST)
+    if form.is_valid():
+        list_ = List.objects.create()
+        form.save(for_list=list_)
+        return redirect(list_)
+    else:
+        return render(request, 'home.html', {"form": form})
