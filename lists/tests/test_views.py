a238c755ceb6d66d68dd137ce1ccd90319a2b355
focus-sh <wangzhibin2013@126.com>
2020-03-08 14:36:59 +0800

    Valiate duplicate item with complicate form

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -4,7 +4,7 @@ from lists.views import home_page
 from django.http import HttpRequest
 from django.utils.html import escape
 from lists.models import Item, List
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import ItemForm, ExistingListItemForm, EMPTY_ITEM_ERROR
 from unittest import skip
 
 class HomePageTest(TestCase):
@@ -96,7 +96,7 @@ class ListViewTest(TestCase):
 
 	def test_for_invalid_input_passes_form_to_template(self):
 		response = self.post_invalid_input()
-		self.assertIsInstance(response.context['form'], ItemForm)
+		self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
 	def test_for_invalid_input_shows_error_on_page(self):
 		response = self.post_invalid_input()
@@ -105,15 +105,14 @@ class ListViewTest(TestCase):
 	def test_displays_item_form(self):
 		list_ = List.objects.create()
 		response = self.client.get(f'/lists/{list_.id}/')
-		self.assertIsInstance(response.context['form'], ItemForm)
+		self.assertIsInstance(response.context['form'], ExistingListItemForm)
 		self.assertContains(response, 'name="text"')
 
-	@skip
 	def test_deplicate_item_validation_errors_end_up_on_lists_page(self):
 		list1 = List.objects.create()
 		item1 = Item.objects.create(list=list1, text='textey')
 		response = self.client.post(
-			f'/lists/{list1.id}',
+			f'/lists/{list1.id}/',
 			data={'text': 'textey'}
 		)
 
