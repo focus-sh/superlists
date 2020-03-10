9c5dd5999b5e440824465b5d4b99f91c7d565c6c
focus-sh <wangzhibin2013@126.com>
2020-03-10 17:09:14 +0800

    Finish study of 20200310

---
+++ accounts/views.py
@@ -0,0 +1,18 @@
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect
+
+
+def send_login_email(request):
+    email = request.POST['email']
+    send_mail(
+        'Your login link for Superlists',
+        'body text tbc',
+        'noreply@superlists',
+        [email]
+    )
+    messages.success(
+        request,
+        "Check your email, we've sent you a link you can use to log in."
+    )
+    return redirect('/')
