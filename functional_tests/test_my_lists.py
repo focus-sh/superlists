cf3b0b9d478fa7039e5e38ab73d567622448a801
focus-sh <wangzhibin2013@126.com>
2020-03-13 18:10:05 +0800

    Firday is off

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.contrib.sessions.backends import file
 
 from functional_tests.base import FunctionalTest
 from functional_tests.management.commands.create_session import create_pre_authenticated_session
@@ -31,3 +32,40 @@ class MyListTest(FunctionalTest):
         self.create_pre_authenticated_session(email)
         self.browser.get(self.live_server_url)
         self.wait_to_be_logged_in(email)
+
+    def test_logged_in_users_lists_are_saved_as_my_lists(self):
+        self.create_pre_authenticated_session('edith@example.com')
+        self.browser.get(self.live_server_url)
+        self.add_list_item('Reticulate splines')
+        self.add_list_item('Immanentize eschaton')
+        first_list_url = self.browser.current_url
+
+        self.browser.find_element_by_link_text('My lists').click()
+
+        self.wait_for(
+            lambda: self.browser.find_element_by_link_text('Reticulate splines')
+        )
+        self.browser.find_element_by_link_text('Reticulate splines').click()
+        self.wait_for(
+            lambda: self.assertEqual(self.browser.current_url, first_list_url)
+        )
+
+        self.browser.get(self.live_server_url)
+        self.add_list_item('Click cows')
+        second_list_url = self.browser.current_url
+
+        self.browser.find_element_by_link_text('My lists').click()
+        self.wait_for(
+            lambda: self.browser.find_element_by_link_text('Click cows')
+        )
+        self.browser.find_element_by_link_text('Click cows').click()
+        self.wait_for(
+            lambda: self.assertEqual(self.browser.current_url, second_list_url)
+        )
+
+        self.browser.find_element_by_link_text('Log out').click()
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_link_text('My lists'),
+            []
+        ))
+
